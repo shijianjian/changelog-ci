ef8298de4b72459f09fe7836db9ca4dcf2139f95
shijianjian <sj8716643@126.com>
2020-10-29 17:04:25 +0800

    Update changelog-ci.py

--- scripts/changelog-ci.py
+++ scripts/changelog-ci.py
@@ -379,7 +379,7 @@ class ChangelogCI:
             return
 
         string_data = self._parse_data(version, pull_request_data)
-        _print_output('info', msg)
+        _print_output('info', string_data)
 
         if self.config['commit_changelog']:
             subprocess.run(['echo', '::group::Commit Changelog'])
